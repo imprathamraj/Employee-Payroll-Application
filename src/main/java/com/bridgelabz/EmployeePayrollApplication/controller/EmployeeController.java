22c129aecc3b0aecb3ff1da0da17c78a995b04e4
imprathamraj <rjpratham05@gmail.com>
2025-02-24 15:55:27 +0530

    [Pratham Raj] Fix. EmployeeController to add logs

--- src/main/java/com/bridgelabz/EmployeePayrollApplication/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.bridgelabz.EmployeePayrollApplication.controller;
 
 import com.bridgelabz.EmployeePayrollApplication.model.Employee;
 import com.bridgelabz.EmployeePayrollApplication.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +14,42 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
     @Autowired
     EmployeeService employeeService;
     //To get all the employees
     @GetMapping
     public List<Employee> getAllEmployees(){
+        logger.info("All employee endpoint called");
         return employeeService.getAllEmployee();
     }
 
     //TO get employee with id
     @GetMapping("/get/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable Long id){
+        logger.info("By id employee endpoint called");
         return employeeService.getEmployeeById(id);
     }
 
     @PostMapping("/create")
     public Employee addEmployee(@RequestBody Employee employee){
+        logger.info("Create employee endpoint called");
         return employeeService.addEmployee(employee);
     }
 
     //Update the employee
     @PutMapping("/update/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        logger.info("Update employee endpoint called");
         return employeeService.updateEmployee(id,employee);
     }
 
     //Delete the employee
     @DeleteMapping("/delete/{id}")
     public void deleteEmployee(@PathVariable Long id){
+        logger.info("Delete employee endpoint called");
         employeeService.deleteEmployee(id);
     }
 
