57695d35ee33ea1d9b599fe290c8413b6062e2ca
imprathamraj <rjpratham05@gmail.com>
2025-02-27 11:48:01 +0530

    [Pratham Raj] Add. ErrorResponseDTO

---
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/dto/ErrorResponseDTO.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.EmployeePayrollApplication.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDTO {
+    private String message;
+    private String details;
+    private LocalDateTime timestamp;
+
+    public ErrorResponseDTO(String message, String s) {
+        this.message = message;
+        this.details = details;
+        this.timestamp = LocalDateTime.now();
+    }
+}
