70f14d9d1edd7a547e19ac895e145bd37a37771a
imprathamraj <rjpratham05@gmail.com>
2025-02-27 11:49:27 +0530

    [Pratham Raj] Add. Ensuring validation is done on the Payroll DTO

--- src/main/java/com/bridgelabz/EmployeePayrollApplication/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/dto/EmployeeDTO.java
@@ -2,12 +2,13 @@ package com.bridgelabz.EmployeePayrollApplication.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeDTO {
+public  @ToString class EmployeeDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     private String name;
@@ -15,13 +16,23 @@ public class EmployeeDTO {
     @Min(value = 10000, message = "Salary should be minimum 10,000")
     private double salary;
 
+    @Pattern(regexp = "Male|Female", message = "Gender needs to be male or female only")
     private String gender;
 
-    private String startDate;
+    // Date is converted into LocalDate with @JsonFormat and pattern
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "Start date should not be set empty")
+    // To take only past or today's date
+    @PastOrPresent(message = "Start date should be past or today's date")
+    private LocalDate startDate;
 
+    @NotBlank(message = "Note can not be set empty")
     private String note;
 
+    @NotBlank(message = "Profile picture can not be set empty")
     private String profilePic;
 
+    @NotNull(message = "Department can not be set empty!")
     private String department;
+
 }
--- src/main/java/com/bridgelabz/EmployeePayrollApplication/exception/EmployeeGlobalExceptionHandler.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/exception/EmployeeGlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.bridgelabz.EmployeePayrollApplication.exception;
 
+import com.bridgelabz.EmployeePayrollApplication.dto.EmployeeDTO;
+import com.bridgelabz.EmployeePayrollApplication.dto.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class EmployeeGlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,9 +23,19 @@ public class EmployeeGlobalExceptionHandler {
                 errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeExceptions(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    private static final String message = "Exception while processing REST Request";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleHTTPMessageNotReadableException(HttpMessageNotReadableException exception) {
+        log.error("Invalid Input Format", exception);
+        ErrorResponseDTO responseDTO = new ErrorResponseDTO(message, "Should have date in the format dd MMM yyyy");
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
 }
